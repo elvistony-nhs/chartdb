1d17d3705018ee69b050b65543e21eb9088ce69c
elvistony-nhs <elvis.tony@nhs.net>
2026-09-23 11:11:59 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/qbits/chartdb/',
     plugins: [
         react(),
         visualizer({
